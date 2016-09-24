244b9bdf62039fc38ddeabbea6e1728e7fac1d63
Dylan DeAmaral <deamaral.dylan@gmail.com>
2016-09-24 15:44:47 -0400

    pushing updates to progress in the book

--- PythonPTVS/LearningPython/LearningPython/Chapter2.py
+++ PythonPTVS/LearningPython/LearningPython/Chapter2.py
@@ -1,6 +1,6 @@
-#Workthrough the book of Python Crash Course Chapter 2
+﻿#Workthrough the book of Python Crash Course Chapter 2 By Eric Matthes
 
-###############Variables##########################
+############### String Variables ##########################
 
 # Condensed information from the book on 'naming and using vairables'
 # You should follow some rules when naming your variables in Python
@@ -12,8 +12,72 @@
 # Example: Name is better than n, student_name is better than s_n, name_length is better than length_of_persons_name
 
 
-message = "Hello Python world!" #assign a message to the variable 'message'
+message = "Hello Python world!" #assigning a message to the variable 'message' so later it can be printed to the console
 print(message)
 
-message = "Hello Python crash Course World!" # we re assigned the value of the 'message' variable and reprinted
-print(message)
+message = "Hello Python crash Course World!" # we re assigned the value of the 'message' variable and reprinted so it would have a different message printed
+print(message) 
+
+
+############### Numeric Variables ##########################
+
+### Integers ###
+Sum = 2 + 3  #display of how to add
+difference = 3 - 2 #display of how to subtract
+product = 2 * 3 #display of how to multiply
+quotient = 3/2 #display of how to divide
+
+ThreeSquared = 3**2 # how to square a number
+ThreeCubed = 3**3 # how to cube a number
+ToTheSixth = 10**6 #example of how to raise to any exponent
+
+
+### Order of Operations ###
+ExpressionResult = 2 + 3 * 4 # note the order of operations is supported in python
+ExpressionResult2 = (2 + 3) * 4 # further demoing order of operations
+
+
+### Floats ###
+
+#Important note : Python declares any number with a decimal point a float (a number that can have a decimal point in any position)
+
+FloatAddition = 0.1 + 0.1
+FloatAddition2 = 0.2 + 0.2
+FloatMultiply = 2 * 0.1
+FloatMultiply2 = 2 * .02
+
+#Beware that Python like most other languages has to store numbers to be as precise as possible due to the way numbers are stored
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+#Avoid type errors with the str() function
+age = 23
+message = "Happy " + str( age ) + "rd Birthday!"
+#print(message) # if uncommented, will throw an error: Can't convert 'int' object to str implicitly 
+
+#Note, in python 2, division of numbers is different. 3 / 2 in python 2 results in 1 instead of 1.5
+#To avoid this, make sure at least one of your number in your expression has a decimal point
+
+#################### Very important information about commenting ##########################
+# What Kind of Comments Should You Write? The main reason to write 
+# comments is to explain what your code is supposed to do and how
+# you are making it work. When you’re in the middle of working on a project, 
+# you understand how all of the pieces fit together. But when you return to a project
+# after some time away, you’ll likely have forgotten some of the details. You can always
+# study your code for a while and figure out how segments were supposed to work, 
+# but writing good comments can save you time by summarizing your overall approach 
+# in clear English. If you want to become a professional programmer or collaborate
+# with other programmers, you should write meaningful comments. 
+# Today, most software is written collaboratively, whether by a group of employees 
+# at one company or a group of people working together on an open source project. 
+# Skilled programmers expect to see comments in code, so it’s best to start adding
+# descriptive comments to your programs now. Writing clear, concise comments in your code is
+# one of the most beneficial habits you can form as a new programmer. When you’re determining
+# whether to write a comment, ask yourself if you had to consider several approaches before
+# coming up with a reasonable way to make something work; if so, write a comment about your
+# solution. It’s much easier to delete extra comments later on than it is to go back and
+# write comments for a sparsely commented program. From now on, I’ll use comments in examples
+# throughout this book to help explain sections of code.
+
+import this # This will output the basic rules of Python that you should try to follow.
+ 
--- PythonPTVS/LearningPython/LearningPython/Chapter3.py
+++ PythonPTVS/LearningPython/LearningPython/Chapter3.py
@@ -1,14 +1,73 @@
-#this is a display of Chapter 3 Lists of Python crash Course by Eric Matthes
+﻿#this is a display of Chapter 3 Lists of Python crash Course by Eric Matthes
 
-myMotorcycle = 'Harley' #string variable
-motorcycles = ['Honda', 'yamaha', 'suzuki'] #list of motorcycles
-motorcycles.insert(0, myMotorcycle) #inject my motorcycle at index 0
+# Important information from the book
+# "what Is a list? A list is a collection of items in a particular order. You can make a list that includes the letters of the alphabet, 
+# the digits from 0–9, or the names of all the people in your family. You can put anything you want into a list, and 
+# the items in your list don’t have to be related in any particular way. 
+# Because a list usually contains more than one element, it’s a good idea to make the name of your list plural, such as letters, digits, or names"
+#
+#
+#
+#
+#
+#
 
-print(motorcycles)# print results
+bicycles = ['trek', 'cannondale', 'redline', 'specialized'] # you can hold multiple items in a list, strins, numbers and objects.
+print(bicycles) # Python can print lists for you, but the output isn't so pretty
+print(bicycles[0]) # you can access a list by a 0 based index like so. This prints out the first bicycle.
+print(bicycles[0].title())# the output of bicycles[0] is a string, so you can use string methods on this element as shown by using .title()
+print(bicycles[1]) # a position in the list is 0 based, meaning if you want the second item, you say bicycles[1] instead of bicycles[2]
+print(bicycles[3]) # this is true for most programming languages
 
-del motorcycles[3] # if you know the position of your element, you can use the del keyword to delete it
+### Useful tip: if you have a list you don't know the index of, you can get the last index by asking for -1 ###
+print(bicycles[-1]) # Python has a special syntax for asking for the last item in a list, by accessing the index of -1
 
-print(motorcycles)#reprint to show delete
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
 
 
+### Try it youself task 3-1 ###
+#       3-1. Names: Store the names of a few of your friends in a list called names . Print each person’s
+#   name by accessing each element in the list, one at a time .
+friends = ['Sam', 'Mike', 'Brad', 'Alvaro', 'Blaise','Carlton'] # creatng a list of friends to print
+# print out each friends name to practice list accessing
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[-1])
+
+message = "Hello " + friends[0].title()
+print(message)
+
+### Try it yourself 3-3
+#   3-3 Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a 
+#car, and make a list that stores several examples . 
+#Use your list to print a series of statements about these 
+#items, such as “I would like to own a Honda motorcycle .”
+
+message = "My girlfriend's name is "  + friends[0].title() + "." #example of how to put list elements in a sentence
+print message
+
+message = "My friend " + friends[1].title() + " was in the USMC." #another example of putting an element in a sentence
+print message
+
+#### Changing, adding, and removing elements  #####
+motorcycles = ['Honda', 'yamaha', 'suzuki'] #list of motorcycles to practice modifying lists
+print motorcycles # just show what is in the list so far
+
+my_motorcycle = 'Harley' #this is a variable that holds a string, which saves in memory
+motorcycles[0] = my_motorcycle # reassigning the first element to the value of my_motorcycle to show how to overwrite a value
+
+print(motorcycles) # showing the change
+motorcycles.insert(0, my_motorcycle) #inject my motorcycle at index 0
+
+print(motorcycles)# print results to show insert
+
+del motorcycles[3] # if you know the position of your element, you can use the del keyword to delete it, this shows it
+
+print(motorcycles)#reprint to show deletion of motorcycle
+
+print(motorcycles[0].title())
 
